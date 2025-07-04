41d6def9fb050cdb670ab4ad3578c9d621306124
clone4everyone <karanrawat9149@gmail.com>
2025-07-04 17:30:14 +0530

    changed API_BASE_URL

--- frontend/src/contexts/ApiContext.jsx
+++ frontend/src/contexts/ApiContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 const ApiContext = createContext()
 
 // Configure axios defaults
-const API_BASE_URL = 'https://restapi-ven0.onrender.com/api'
+const API_BASE_URL = 'https://restapi-n966.onrender.com'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
